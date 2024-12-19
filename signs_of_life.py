e14687ea4d143ab67bd3f986d291346beb27abec
Laurence Molloy <laurence.molloy@dtime.ai>
2024-12-19 21:28:09 +0000

    auto-update of state.txt

--- signs_of_life.py
+++ signs_of_life.py
@@ -1,3 +1,10 @@
+# UV integration ?
+#1. Create a lock file via push - will fail if updated by someone else
+#2. read state from repo via API
+#3. IF state needs to change, write state back to repo via force push
+#4. Remove lockfile
+
+
 import os
 import time
 import smtplib
@@ -6,62 +13,218 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import subprocess
 from datetime import datetime
+import requests
+import base64
 
 from dotenv import dotenv_values, find_dotenv
 
 # [active]->-send inactive->-[inactive]->-event->-[waking]->-send wake->-[active]
 
-# File to update
-TIMESTAMP_FILE = "last_active.txt"
-
 # --- Settings ---
 env = dotenv_values(find_dotenv())
-CHECK_INTERVAL = int(env['CHECK_INTERVAL_SECONDS'])  # Inactivity threshold in seconds
-ALERT_INTERVAL = int(env['ALERT_INTERVAL_SECONDS'])  # Inactivity threshold in seconds
+CHECK_INTERVAL = int(env['CHECK_INTERVAL_SECONDS']) 
+ALERT_INTERVAL = int(env['ALERT_INTERVAL_SECONDS']) 
 EMAIL_ADDRESS = env['SENDER_EMAIL_ADDRESS']
-EMAIL_PASSWORD = env['SENDEREMAIL_PASSWORD']  # Replace with app password
+EMAIL_PASSWORD = env['SENDER_EMAIL_PASSWORD']
 RECIPIENT_EMAIL = env['RECIPIENT_EMAIL_ADDRESS']
+REPO_URL = env['REPO_URL']
+GITHUB_TOKEN = env['GITHUB_TOKEN']
+BRANCH = env['BRANCH']
+TIMESTAMP_FILE = env['TIMESTAMP_FILE']
+STATE_FILE = env['STATE_FILE']
+
+# WARNING: this is not write-concurrency-safe (multiple devices simultaneously updating state)
+STATE_URL = env['STATE_URL']
 
 # --- StateMachine Class ---
 class StateMachine:
     def __init__(self, alert_interval):
-        self.state = "active"  # Initial state
+        #self.state = "blobby"  # Initial state
+        self.state = "qwertyxxx"  # Initial state
+        print(self.state)
+        exit()
         self.last_activity = time.time()
         self.alert_interval = alert_interval
         self.update_timestamp() # initialise timestamp upon start up
+        self.update_state('active') # initialise state upon start up
+        self.push_to_repo([STATE_FILE, TIMESTAMP_FILE])
+
+    @property
+    def state(self):
+        """
+        returns the content of the repository state file located at STATE_URL.
+        STATE_URL is configurable in the .env file.
+        
+        Returns:
+            str: The content of the state file if successful, None otherwise.
+        """
+        headers = {
+            "Authorization": f"Bearer {GITHUB_TOKEN}"
+        } if GITHUB_TOKEN else {}
+
+        # Fetch state file metadata
+        response = requests.get(f"{STATE_URL}?token={time.time()}", headers=headers)
+        if response.status_code != 200:
+            print(f"Error fetching file metadata: {response.status_code}")
+            return None
+
+        try:
+            # Parse the metadata and extract the download URL and SHA
+            data = response.json()
+            file_content_url = data.get("download_url")
+
+            if not file_content_url:
+                print("Download URL not found in response.")
+                return None
+
+            # Fetch raw file content
+            file_response = requests.get(file_content_url)
+            if file_response.status_code == 200:
+                return file_response.text
+            else:
+                print(f"Error fetching raw file content: {file_response.status_code}")
+                return None
+        except (ValueError, KeyError) as e:
+            print(f"Error parsing response: {e}")
+            return None
+
+    @state.setter
+    def state(self, state):
+        with open(STATE_FILE, "w") as f:
+            f.write(f"Last activity: {state}")
+        self.push_to_repo(STATE_FILE)
+
+    def put_state(self, state):
+        headers = {
+            "Authorization": f"Bearer {GITHUB_TOKEN}",
+            "Accept": "application/vnd.github+json"
+        } if GITHUB_TOKEN else {}
+
+        # File content and commit message
+        content = state
+        encoded_content = base64.b64encode(content.encode()).decode()
+        commit_message = f"updating state to {state}"
+        state_sha = self.state_sha
+
+        # Create or update the file
+        data = {
+            "message": commit_message,
+            "content": encoded_content,
+            "branch": BRANCH
+        }
+        if state_sha:
+            data["sha"] = state_sha # if updating, include the SHA
+
+        response = requests.put(STATE_URL, headers=headers, json=data)
+
+        if response.status_code in (200, 201):
+            return state
+        else:
+            print("Failed to update file:", response.json())
+
+
+    @property
+    def state_sha(self):
+        """
+        returns the SHA of the repository state file located at STATE_URL.
+        STATE_URL is configurable in the .env file.
+        
+        Returns:
+            str: The SHA of the state file if successful, None otherwise.
+        """
+        headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"} if GITHUB_TOKEN else {}
+
+        # Fetch state file metadata
+        response = requests.get(STATE_URL, headers=headers)
+        if response.status_code != 200:
+            print(f"Error fetching file metadata: {response.status_code}")
+            return None
+
+        try:
+            # Parse the metadata and extract the download URL and SHA
+            data = response.json()
+            state_sha = data.get("sha") 
+
+            if not state_sha:
+                print("SHA not found in response.")
+                return None
+            else:
+                return state_sha
+
+        except (ValueError, KeyError) as e:
+            print(f"Error parsing response: {e}")
+            return None
+
+
+    def update_activity(self):
+        """Update the last activity timestamp and handle waking state."""
+        self.last_activity = time.time()
+        if self.state == "inactive":
+            self.update_state("waking")
+
+    def update_state(self, new_state):
+        """Change state and perform actions if necessary."""
+        # only act on state CHANGE
+        if new_state == "inactive" and self.state == "active":
+            self.send_email(
+                "User Inactivity Alert",
+                f"No user activity detected in the past {self.alert_interval} seconds.",
+            )
+        # only act on state CHANGE
+        elif new_state == "waking" and self.state == "inactive":
+            self.send_email("User Waking Alert", "User has woken up.")
+        self.state = new_state
+        with open(STATE_FILE, "w") as f:
+            f.write(f"{self.state}\n")
+
+    def check_inactivity(self):
+        """Check for inactivity and transition states."""
+        current_time = time.time()
+        if current_time - self.last_activity > self.alert_interval:
+            self.update_state("inactive")
+            self.push_to_repo([STATE_FILE])
+        elif self.state == "waking":
+        #    self.update_state("active")
+            self.update_timestamp()
+            self.push_to_repo([STATE_FILE, TIMESTAMP_FILE])
+        else:
+            self.update_timestamp()
+            self.push_to_repo([STATE_FILE, TIMESTAMP_FILE])
 
     def update_timestamp(self):
         with open(TIMESTAMP_FILE, "w") as f:
             current_time = datetime.now().isoformat()
             f.write(f"Last activity: {current_time}\n")
 
-    def update_activity(self):
-        """Update the last activity timestamp and handle waking state."""
-        self.last_activity = time.time()
-        if self.state == "inactive":
-            self.change_state("waking")
+    def push_to_repo(self, file):
+        """Push updates to the repository."""
+        repo_url_with_token = REPO_URL.replace("https://", f"https://{GITHUB_TOKEN}@")
+        try:
+            subprocess.run(["git", "add", file], check=True)#,
+                           #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(["git", "commit", "-m", f"auto-update of {file}"], check=True)#,
+                           #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # force push is safe as knowledge of current state is updated from the repo file via API
+            subprocess.run(["git", "push", "--force", repo_url_with_token, BRANCH],  check=True)#,
+                           #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            print("Changes pushed successfully!")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to push changes: {e}")
 
-    def change_state(self, new_state):
-        """Change state and perform actions if necessary."""
-        if new_state == "inactive" and self.state == "active":
-            self.send_email(
-                "User Inactivity Alert",
-                f"No user activity detected in the past {self.alert_interval} seconds.",
-            )
-        elif new_state == "active" and self.state == "waking":
-            self.send_email("User Waking Alert", "User has woken up.")
-        self.state = new_state
-
-    def check_inactivity(self):
-        """Check for inactivity and transition states."""
-        current_time = time.time()
-        if current_time - self.last_activity > self.alert_interval:
-            self.change_state("inactive")
-        elif self.state == "waking":
-            self.change_state("active")
-            self.update_timestamp()
-        else:
-            self.update_timestamp()
+#    def push_to_repo(self, file_list):
+#        """Push updates to the repository."""
+#        repo_url_with_token = REPO_URL.replace("https://", f"https://{GITHUB_TOKEN}@")
+#        try:
+#            subprocess.run(["git", "add", *file_list], 
+#                           stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+#            subprocess.run(["git", "commit", "-m", f"auto-update of {STATE_FILE} and {TIMESTAMP_FILE}"],
+#                           stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+            # force push is safe as knowledge of current state is updated from the repo file via API
+#            subprocess.run(["git", "push", "--force", repo_url_with_token, BRANCH], 
+#                           stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+#            print("Changes pushed successfully!")
+#        except subprocess.CalledProcessError as e:
+#            print(f"Failed to push changes: {e}")
 
     def send_email(self, subject, body):
         """Send an email notification."""
@@ -101,6 +264,9 @@ def main():
 
     # Periodically check inactivity
     while True:
+        print(state_machine.state)
+        state_machine.state = 'blob'
+        exit()
         state_machine.check_inactivity()
         time.sleep(CHECK_INTERVAL)  # Check more frequently than the alert threshold
 
